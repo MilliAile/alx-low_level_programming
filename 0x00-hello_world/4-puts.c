102645830734776768dc8647065e688e93635d52
MilliAilecd <millisima45@gmail.com>
2022-12-08 04:25:49 -0800

    hello betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+/**
+ * cprograming inbetty style
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
