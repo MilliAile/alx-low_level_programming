15d1c7bb9483fc5c51b1bfe5b8b8754398176346
MilliAilecd <millisima45@gmail.com>
2022-12-12 19:12:31 -0800

    betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -10,7 +10,7 @@ int main(void)
 
 	c = 'a';
 	while
-	       	(c <= 'z') {
+		(c <= 'z') {
 		putchar(c);
 		c++;
 	}
