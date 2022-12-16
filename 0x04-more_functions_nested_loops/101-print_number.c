39ad6e62df48e419182a4d7a7fe8bf3226f46cc1
MilliAilecd <millisima45@gmail.com>
2022-12-15 19:26:34 -0800

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
 void print_number(int n)
 {
 	unsigned int i, j, count;
-	
+
 	if (n < 0)
 	{
 		_putchar(45);
@@ -20,8 +20,8 @@ void print_number(int n)
 	j = i;
 	count = 1;
 	while (j > 9)
-	{ 
-		j/= 10;
+	{
+		j /= 10;
 		count *= 10;
 	}
 	for (; count >= 1; count /= 10)
