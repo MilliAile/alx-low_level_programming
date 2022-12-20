6b11a743010cc1e8a6ac3478bef80dadc370751d
MilliAilecd <millisima45@gmail.com>
2022-12-19 23:56:53 -0800

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,20 @@
 #include "mill.h"
+#include <stdio.h>
 /**
- *
- *
- *
+ * print_array - print the arrays
+ * @a: the pointer
+ * @n: the number of the elements
+ * Return: nothing
  */
 void print_array(int *a, int n)
 {
+	int i = 0;
 
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
 }
