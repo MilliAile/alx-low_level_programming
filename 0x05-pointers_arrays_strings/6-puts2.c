f2ef2c0a8d3332f21e31a2766da597cbe2525200
MilliAilecd <millisima45@gmail.com>
2022-12-19 18:44:14 -0800

    print other stringsfrom the first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,20 @@
 include "mill.h"
 /**
- *
- *
- *
- *
+ * puts2 - print string starting from the first
+ * @str: the string variable
+ * Return: nothing
  */
 void puts2(char *str);
 {
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
