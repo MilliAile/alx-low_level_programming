db753e035d19d4f7f7c35f9c7dbf378cb3366813
MilliAilecd <millisima45@gmail.com>
2022-12-19 17:05:13 -0800

    stringlength

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - length of string
  * @s: string
- * Return: nothing
+ * Return: success 0.
  */
 
 int _strlen(char *s)
@@ -14,5 +14,6 @@ int _strlen(char *s)
 	{
 		i++;
 	}
+	return (0);
 
 }
