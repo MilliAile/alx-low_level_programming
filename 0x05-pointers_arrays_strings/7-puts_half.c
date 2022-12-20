078df4688b9ff40168b2ee31042a8703ac06aae7
MilliAilecd <millisima45@gmail.com>
2022-12-19 19:06:15 -0800

    halfofstring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints a string
+ * @str: the sting pointer
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int len, n, i;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; i++)
+		{
+			_puchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
