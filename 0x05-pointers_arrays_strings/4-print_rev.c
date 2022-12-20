c4c63bf78e26b3889b40ef27ebf0b04ac78b029f
MilliAilecd <millisima45@gmail.com>
2022-12-19 17:39:30 -0800

    reverse_of_string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "mill.h"
+/**
+ * print_rev - print the reverse of string
+ * @s: the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i])
+		i++;
+	for (int j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
