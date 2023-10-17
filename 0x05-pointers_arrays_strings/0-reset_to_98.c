89508029e5bd7b934744d287457a92727674c1a3
MilliAile <millisima45@gmail.com>
2023-10-17 17:45:00 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 5;
+
+	reset_to_98(&a);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef <MAIN.H>
+#define <MAIN.H>
+#include <stdio>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
