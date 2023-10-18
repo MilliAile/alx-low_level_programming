3bc91a11f1cc079223bc5f5d251d9d89474b216c
MilliAile <millisima45@gmail.com>
2023-10-18 06:56:53 +0000

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _puts - function to print string
+ * @str: the address of the first string element
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		putchar('\n');
+		str++;
+	}
+}
