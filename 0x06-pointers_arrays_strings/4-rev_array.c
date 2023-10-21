6c2fa90da06f40727eac12362490a46897d5d649
MilliAile <millisima45@gmail.com>
2023-10-21 17:48:21 +0000

    reverse array of num

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,24 +8,15 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	for (i = n - 1; i >= 0; i--)
+	i = n - 1;
+	while (i >= 0)
 	{
 		printf("%d", a[i]);
 		if (i != 0)
 		{
 			printf(", ");
 		}
-
-	}
-	/**i = n - 1;
-	while (i >= 0)
-	{
-		printf("%d", a[i]);
-		if (i != 0);
-		{
-			printf(", ");
-		}
 		i--;
-	}*/
+	}
 	printf("\n");
 }
