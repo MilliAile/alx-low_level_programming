51a0149a0763947257dffaf58081fd36b626c94b
MilliAile <millisima45@gmail.com>
2023-10-21 01:55:19 +0000

    copy number of string elements onto the other string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <string.h>
 /**
+ * _strncpy - function to copy n element of string
+ * @dest: string to be copied upon
+ * @src: string to be copied
+ * @n: number of elements
+ * Return: dest - new value
  */
 char *_strncpy(char *dest, char *src, int n)
 {
