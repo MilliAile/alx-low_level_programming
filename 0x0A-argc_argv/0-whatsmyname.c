58b981c9f7b15b1dbd68795006cb57af9d5cc00b
MilliAile <millisima45@gmail.com>
2023-10-27 14:15:53 +0000

    print the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: an array containing commandline arguement
  * Return: argc
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
