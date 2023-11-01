cd46d84ffe7987764d3b33c0759261bac503cc86
MilliAile <millisima45@gmail.com>
2023-11-01 15:19:07 +0000

    duplicate of string that can be deallocated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function to return duplicate of string
+ * @str: the string to be duplicated
+ * Return: return the duplicate string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, j;
+
+	j = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = strlen(str);
+	dup = malloc(sizeof(char) * i);
+	while (j <= i)
+	{
+		dup[j] = str[j];
+		j++;
+	}
+	return (dup);
+}
