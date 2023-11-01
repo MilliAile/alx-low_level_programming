3e566f889107fef3ecd517938da2064d92501e7f
MilliAile <millisima45@gmail.com>
2023-11-01 14:05:00 +0000

    array of specific charactor

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - function that returns array of a specific char
+ * @size: the size of an array and the memory to be allocated
+ * @c: the specific char of the array
+ * Return: string or the array of the specific char
+ * malloc - function that allacate the memory for the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	buffer = malloc(size * sizeof(char));
+	for (i = 0; i < 98; i++)
+	{
+		buffer[i] = c;
+	}
+	return (buffer);
+}
