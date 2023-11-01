c2ebd3a231a66b0230b87e1d231b15e221089d3f
MilliAile <millisima45@gmail.com>
2023-11-01 14:35:17 +0000

    array of specific charactor

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	buffer = malloc(size * sizeof(char));
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		buffer[i] = c;
