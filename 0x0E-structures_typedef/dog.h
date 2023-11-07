62a4d54f41d03877ab541dd004beb019d3d88867
MilliAile <millisima45@gmail.com>
2023-11-07 20:49:24 +0300

    dog's identifiers

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+/**
+ * struct dog - structure that define the dogs identity
+ * @name: the name of the dog
+ * @age: the dog's age
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
