b05612da8c39ce4e13ef75092ff643c691eb7065
MilliAile <millisima45@gmail.com>
2023-11-09 05:58:40 +0300

    simple calculation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,7 +7,7 @@
  * @argv: the content of commant line arguement contained in an array
  * Return: 0 on success, and 98, 99 or 100 on error
  */
-int main(int __attribute__((__unused__))argc,char *argv[])
+int main(int argc,char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -17,9 +17,9 @@ int main(int __attribute__((__unused__))argc,char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[2]);
-	op = argv[3];
-	num2 = atoi(argv[4]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
