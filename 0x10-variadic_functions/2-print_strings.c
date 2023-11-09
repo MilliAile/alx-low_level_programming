17ad594369e5cc807fd74e3279885b207c9cadab
MilliAile <millisima45@gmail.com>
2023-11-09 20:49:55 +0300

    print strings with a string separator

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,11 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", string);
 		}
-		if (separator == NULL)
-		{
-			continue;
-		}
-		else if (i < (n - 1))
+		if (separator != NULL && i < (n - 1))
 		{
 			printf("%s", separator);
 		}
