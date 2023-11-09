82f72778cab0c654e50fa8b64d843cc825384467
MilliAile <millisima45@gmail.com>
2023-11-09 09:56:06 +0300

    print strings with a string separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - function that returns strings with separator
+ * @separator: string to separate parameter strings
+ * @n: the expected number of parameters
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *string;
+	va_list st;
+
+	va_start(st, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(st, char*);
+		if (string == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", string);
+		}
+		if (separator == NULL)
+		{
+			continue;
+		}
+		else if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
