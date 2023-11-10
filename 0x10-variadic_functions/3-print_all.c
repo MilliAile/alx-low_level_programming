9edcd1bdbb6a8ab8c7026e03244c9774a7530a45
MilliAile <millisima45@gmail.com>
2023-11-10 05:59:29 +0300

    print all four types

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_all - print all type
+ * @format: the format of the args is given
+ */
+void print_all(const char * const format, ...)
+{
+	int length, a, i;
+	char c;
+	double f;
+	char *string;
+	va_list type;
+
+	va_start(type, format);
+	length = strlen(format);
+	for (i = 0; i < length; i++)
+	{
+		if (format[i] == 'i')
+		{
+			a = va_arg(type, int);
+			printf("%d", a);
+		}
+		else if (format[i] == 'c')
+		{
+			c = va_arg(type, int);
+			printf("%c", c);
+		}
+		else if (format[i] == 'f')
+		{
+			f = va_arg(type, double);
+			printf("%f", f);
+		}
+		else if (format[i] == 's')
+		{
+			string = va_arg(type, char*);
+			printf("%s", string);
+		}
+		if (i < (length - 1))
+			printf(", ");
+
+	}
+	printf("\n");
+	va_end(type);
+}
