6a662fda988e1f5740bd20ac576a25da464c828f
MilliAile <millisima45@gmail.com>
2023-11-10 21:51:40 +0300

    print them all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	va_list type;
 
 	va_start(type, format);
-	while (format == NULL)
+	while (format == NULL || *format == '\0')
 		return;
 	while (format[i] != '\0')
 	{
